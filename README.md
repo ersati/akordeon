a2a218d0ea461a13ad8394e5375eda407af1f70c
ersati <piotrp92@gmail.com>
2021-07-03 22:51:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# akordeon-h5edz7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/akordeon-h5edz7)
---
+++ index.js
@@ -0,0 +1,27 @@
+// Import stylesheets
+import './style.css';
+
+const wrapper = document.querySelector('.item-wrapper');
+
+const item = document.querySelector('.item');
+const arrow = document.querySelector('.arrow');
+
+console.log(wrapper, item, arrow);
+
+arrow.addEventListener('click', () => {
+  console.log('ok');
+  // arrow.style.background = 'red'
+  // arrow.style.animation = 'roundItem 1s forwards'
+  // item.style.animation = 'hideOut 1s forwards'
+  arrow.classList.toggle('round-item');
+  item.classList.toggle('hide-item');
+  if (item.classList.contains('hide-item')) {
+    setTimeout(function() {
+      item.style.display = 'none';
+    }, 1000);
+  } else {
+    setTimeout(function() {
+      item.style.display = '';
+    }, 1000);
+  }
+});
