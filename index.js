fd837ba3640fca0f221c242cbb9795e385efbb8a
ersati <piotrp92@gmail.com>
2021-07-06 01:54:16 +0100

    download all elements to manipulate

--- index.js
+++ index.js
@@ -14,21 +14,21 @@ const svgs = document.querySelectorAll('article div svg');
 svgs[0].setAttribute('stroke', 'red');
 console.log(articles, texts, bordersArrow);
 
-arrow.addEventListener('click', () => {
-  console.log('ok');
-  // arrow.style.background = 'red'
-  // arrow.style.animation = 'roundItem 1s forwards'
-  // item.style.animation = 'hideOut 1s forwards'
-  arrow.classList.toggle('round-item');
-  item.classList.toggle('hide-item');
-  if (item.classList.contains('hide-item')) {
-    setTimeout(function() {
-      item.style.display = 'none';
-    }, 1000);
-  } else {
-    item.style.display = '';
-  }
-});
+// arrow.addEventListener('click', () => {
+//   console.log('ok');
+//   // arrow.style.background = 'red'
+//   // arrow.style.animation = 'roundItem 1s forwards'
+//   // item.style.animation = 'hideOut 1s forwards'
+//   arrow.classList.toggle('round-item');
+//   item.classList.toggle('hide-item');
+//   if (item.classList.contains('hide-item')) {
+//     setTimeout(function() {
+//       item.style.display = 'none';
+//     }, 1000);
+//   } else {
+//     item.style.display = '';
+//   }
+// });
 
 svgs[0].addEventListener('click', () => {
   borders[0].classList.remove('border-transparent');
@@ -40,3 +40,41 @@ svgs[0].addEventListener('click', () => {
   svgs[0].setAttribute('stroke', 'white');
   bordersArrow[0].classList.add('round-item');
 });
+
+
+svgs[1].addEventListener('click', (e) => {
+  /*elements*/
+  const arrow = e.target
+  const borderArrow = arrow.parentNode;
+  const text = borderArrow.previousElementSibling;
+  const header = borderArrow.parentNode;
+  const border = header.parentNode;
+  const divWithItems = header.nextElementSibling
+  const itemWrapper = divWithItems.firstElementChild
+  const item = itemWrapper.firstElementChild
+/*elements*/
+
+  
+if(divWithItems){
+  arrow.classList.toggle('round-item');
+  item.classList.toggle('hide-item');
+  if (item.classList.contains('hide-item')) {
+    setTimeout(function() {
+      item.style.display = 'none';
+    }, 1000);
+  } else {
+    item.style.display = '';
+  }
+}
+
+border.classList.remove('border-transparent');
+  border.classList.add('border-indigo');
+  border.classList.add('bg-grey-lightest');
+  
+  text.classList.add('text-indigo');
+  borderArrow.classList.add('border-indigo');
+  // svgs[0].setAttribute('stroke', 'white');
+  // borderArrow.classList.add('round-item');
+
+  console.log( itemWrapper, item)
+})
