cafd807d6622d4db3fbc6543799c132a04aae093
ersati <piotrp92@gmail.com>
2021-07-12 18:42:07 +0100

    last commit, delete all consolelogs, fix bugs

--- index.js
+++ index.js
@@ -1,124 +1,24 @@
 // Import stylesheets
 import './style.css';
 
-// Import stylesheets
-
-// const wrapper = document.querySelector('.item-wrapper');
-// window.addEventListener('DOMContentLoaded', event => {
-//   console.log('DOM fully loaded and parsed');
-
-// const item = document.querySelector('.item');
-// const arrow = document.querySelector('.arrow');
-// const articles = document.querySelectorAll('article');
-// const texts = document.querySelectorAll('article div span');
-// const borders = document.querySelectorAll('article > div');
-// const bordersArrow = document.querySelectorAll('article header > div');
 const svgs = document.querySelectorAll('article div svg');
-
-// // svgs[0].setAttribute('stroke', 'red');
-// // console.log(articles, texts, bordersArrow);
-
-// // arrow.addEventListener('click', () => {
-// //   console.log('ok');
-// //   // arrow.style.background = 'red'
-// //   // arrow.style.animation = 'roundItem 1s forwards'
-// //   // item.style.animation = 'hideOut 1s forwards'
-// //   arrow.classList.toggle('round-item');
-// //   item.classList.toggle('hide-item');
-// //   if (item.classList.contains('hide-item')) {
-// //     setTimeout(function() {
-// //       item.style.display = 'none';
-// //     }, 1000);
-// //   } else {
-// //     item.style.display = '';
-// //   }
-// // });
-
-// // svgs[0].addEventListener('click', () => {
-// //   borders[0].classList.remove('border-transparent');
-// //   borders[0].classList.add('border-indigo');
-// //   borders[0].classList.add('bg-grey-lightest');
-
-// //   texts[0].classList.add('text-indigo');
-// //   bordersArrow[0].classList.add('border-indigo');
-// //   svgs[0].setAttribute('stroke', 'white');
-// //   bordersArrow[0].classList.add('round-item');
-// // });
-// function random(name) {
-//   console.log(name);
-// }
-
-// svgs.forEach(item => {
-//   item.addEventListener('click', e => {
-//     /*elements*/
-//     const arrow = e.target;
-//     const borderArrow = arrow.parentNode;
-//     const text = borderArrow.previousElementSibling;
-//     const header = borderArrow.parentNode;
-//     const border = header.parentNode;
-//     const divWithItems = header.nextElementSibling;
-//     const itemWrapper = divWithItems.children[0];
-//     const item = itemWrapper.children[0]
-//       ? divWithItems.children[0]
-//       : 'string';
-//     /*elements*/
-//     console.log(item, itemWrapper, divWithItems);
-//     random(this);
-//     if (divWithItems === null) {
-//       // if (borderArrow.classList.contains('border-indigo')) {
-//       //     arrow.setAttribute('stroke', 'white');
-//       //     borderArrow.classList.add('round-item');
-//       //   } else {
-//       //     arrow.setAttribute('stroke', '#606F7B');
-//       //     borderArrow.classList.remove('round-item');
-//       //   }
-//       return;
-//     }
-//     // console.log(arrow, borderArrow, text, header,border,divWithItems,itemWrapper, item)
-//     if (
-//       document.body.contains(divWithItems) &&
-//       document.body.contains(itemWrapper)
-//     ) {
-//       arrow.classList.toggle('round-item');
-//       item.classList.toggle('hide-item');
-//       if (item.classList.contains('hide-item')) {
-//         setTimeout(function() {
-//           item.style.display = 'none';
-//         }, 1000);
-//       } else {
-//         item.style.display = '';
-//       }
-//     }
-
-//     border.classList.toggle('border-transparent');
-//     border.classList.toggle('border-indigo');
-//     border.classList.toggle('bg-grey-lightest');
-
-//     text.classList.toggle('text-indigo');
-
-//     borderArrow.classList.toggle('border-indigo');
-//     borderArrow.classList.toggle('border-grey');
-//     borderArrow.classList.toggle('bg-indigo');
-
-//     if (borderArrow.classList.contains('border-indigo')) {
-//       arrow.setAttribute('stroke', 'white');
-//       borderArrow.classList.add('round-item');
-//     } else {
-//       arrow.setAttribute('stroke', '#606F7B');
-//       borderArrow.classList.remove('round-item');
-//     }
-
-//     console.log(borderArrow.classList.contains('border-grey'));
-//   });
-// });
-// });
 
 function findElements(element) {
   const arrow = element;
   const borderAroundArrow = element.parentNode;
   const header = element.parentNode.parentNode;
-  const borderAroundSection = element.parentNode.parentNode;
-  return [arrow, borderAroundArrow, header, borderAroundSection];
+  const borderAroundSection = element.parentNode.parentNode.parentNode;
+  const textWrapper = header.nextElementSibling;
+  const mainText = header.firstElementChild;
+
+  return [
+    arrow,
+    borderAroundArrow,
+    header,
+    borderAroundSection,
+    textWrapper,
+    mainText
+  ];
 }
 
 function showText(elements) {
@@ -126,11 +26,33 @@ function showText(elements) {
   const border = elements[1];
   const header = elements[2];
   const borderAroundSection = elements[3];
+  const textWrapper = elements[4];
+  const mainText = elements[5];
+
+  arrow.style.transform = 'rotate(0deg)';
+  arrow.setAttribute('stroke', 'white');
+
   border.classList.remove('border-transparent');
   border.classList.remove('bg-grey-lightest');
+  border.classList.remove('border-grey');
   border.classList.add('border-indigo');
   border.classList.add('bg-indigo');
-  console.log(border);
+  borderAroundSection.classList.remove('border-transparent');
+  borderAroundSection.classList.add('border-indigo');
+  borderAroundSection.classList.add('bg-grey-lightest');
+
+  mainText.classList.remove('text-grey-darkest');
+  mainText.classList.add('text-indigo');
+
+  if (textWrapper == null || textWrapper == undefined) {
+    console.log('element not exist');
+  } else {
+    textWrapper.style.transition = '.5s';
+
+    textWrapper.style.visibility = 'visible';
+    textWrapper.style.opacity = '1';
+    textWrapper.style.height = '100%';
+  }
 }
 
 function hideText(elements) {
@@ -138,22 +60,37 @@ function hideText(elements) {
   const border = elements[1];
   const header = elements[2];
   const borderAroundSection = elements[3];
+  const textWrapper = elements[4];
+  const mainText = elements[5];
+  arrow.setAttribute('stroke', '#606F7B');
 
   border.classList.remove('border-indigo');
   border.classList.remove('bg-indigo');
-  border.classList.add('border-transparent');
   border.classList.add('bg-grey-lightest');
-  console.log('ok');
+  border.classList.add('border-grey');
+
+  borderAroundSection.classList.remove('border-indigo');
+  borderAroundSection.classList.remove('bg-grey-lightest');
+
+  mainText.classList.remove('text-indigo');
+  mainText.classList.add('text-grey-darkest');
+
+  if (textWrapper == null || textWrapper == undefined) {
+    console.log('element not exist');
+  } else {
+    textWrapper.style.transition = '.5s';
+    textWrapper.style.opacity = '0';
+    textWrapper.style.visibility = 'hidden';
+    textWrapper.style.height = '0';
+  }
 }
-svgs.forEach((el, i) => {
-  // const info = findElements(el);
+svgs.forEach(el => {
   const elements = findElements(el);
   if (el.dataset.bool == 'true') {
     showText(elements);
   } else {
     hideText(elements);
   }
-  // hideText(info)
   el.dataset.bool = 'false';
   el.addEventListener('click', () => {
     svgs.forEach((el, i) => {
@@ -161,8 +98,6 @@ svgs.forEach((el, i) => {
       hideText(elements);
     });
     el.dataset.bool = 'true';
-    console.log(el.getAttribute('data-bool'));
-    console.log(i);
     const info = findElements(el);
     showText(info);
   });
